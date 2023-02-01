3c17d1c1251f50509f2f5a6e5f970c1e13be732c
SashaSNky <sanek-avto@yandex.by>
2023-02-02 01:44:40 +0300

    init: task Morse code decoder

--- src/index.js
+++ src/index.js
@@ -38,6 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+    function findLetter(cod, a, b){
+        let i=0;
+        do {
+            if (a[i]===cod) return b[i]
+            else i +=1; 
+        } while (i<=(a.length-1));
+        return 'not found';
+    } //end findLetter
+
+    function getSymbol (str,a, b){ 
+      let res='';
+      if (str[0]=='*') return ' '
+            else {
+                for (let i=0; i<=8; i=i+2) {
+                   ((str[i]=='1')&&(str[i+1]=='0')) ? res+='.':((str[i]=='1')&&(str[i+1]=='1')) ? res+='-' : {}; 
+                }  
+            }
+        return findLetter(res, a, b)    
+    }  //end getSymbol
+  
+    const key = Object.keys(MORSE_TABLE);
+    const symb = Object.values(MORSE_TABLE);
+    var res='';
+    var substr='';
+    for (let i=0; i<=expr.length-10; i+=10){
+        substr=expr.slice(i, i+10);
+        res += getSymbol(substr, key, symb);
+    } 
+    return res;
     // write your solution here
 }
 
